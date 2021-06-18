e4cb7ef48514bd887f12207c195d2ee6029a7ff2
Maria Camila Fetecua <maria.fetecua@mail.escuelaing.edu.co>
2021-06-18 16:50:48 -0500

    Arreglando2

--- src/main/java/edu/escuelaing/arsw/Writer/ChooserR.java
+++ src/main/java/edu/escuelaing/arsw/Writer/ChooserR.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class ChooserR {
     /**
-     * Devuelve el write apartir de un string 
+     * Devuelve el write apartir de un string
      */
     public static Map<String, WriterR> selector=new HashMap<String, WriterR>(){{
         put("html",new TextWriter("html"));
@@ -12,7 +12,7 @@ public class ChooserR {
         put("jpg", (WriterR) new WriterI("jpg"));
         put("js",new TextWriter("javascript"));
         put("css",new TextWriter("css"));
-        put("err", (WriterR) new WriteE("501 Ese tipo no se admite"));
+        put("err", (WriterR) new WriteE("800 Tipo no admitido"));
     }
     };
 
@@ -20,9 +20,9 @@ public class ChooserR {
         String resource="";
         try{
             String[] s=path.split("\\.");
-            resource=s[s.length-1].toLowerCase();           // Existen archivos como owl.min.js
+            resource=s[s.length-1].toLowerCase();
         }catch(ArrayIndexOutOfBoundsException e){
-            throw new Exception(" No es una peticion de Recurso Especifico/ Peticion mal formada");
+            throw new Exception(" Peticion mal formada");
         }
         if (!selector.containsKey(resource)){
             return selector.get("err");
