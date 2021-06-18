8de0ca3ddd72157cc16c87284b792e19a4ea5388
Maria Camila Fetecua <maria.fetecua@mail.escuelaing.edu.co>
2021-06-18 16:43:26 -0500

    Arreglado para heroku

--- src/main/java/edu/escuelaing/arsw/Writer/ChooserR.java
+++ src/main/java/edu/escuelaing/arsw/Writer/ChooserR.java
@@ -7,12 +7,12 @@ public class ChooserR {
      * Devuelve el write apartir de un string
      */
     public static Map<String, WriterR> selector=new HashMap<String, WriterR>(){{
-        put("html",new TextWriter("html"));
-        put("png", (WriterR) new WriterI("png"));
-        put("jpg", (WriterR) new WriterI("jpg"));
-        put("js",new TextWriter("javascript"));
-        put("css",new TextWriter("css"));
-        put("err", (WriterR) new WriteE("501 Ese tipo no se admite"));
+        put("html",new TextWriter(".html"));
+        put("png", (WriterR) new WriterI(".png"));
+        put("jpg", (WriterR) new WriterI(".jpg"));
+        put("js",new TextWriter(".javascript"));
+        put("css",new TextWriter(".css"));
+        put("err", (WriterR) new WriteE("Tipo no admitido"));
     }
     };
 
@@ -20,12 +20,12 @@ public class ChooserR {
         String resource="";
         try{
             String[] s=path.split("\\.");
-            resource=s[s.length-1].toLowerCase();           // Existen archivos como owl.min.js
+            resource=s[s.length-1].toLowerCase();
         }catch(ArrayIndexOutOfBoundsException e){
-            throw new Exception(" No es una peticion de Recurso Especifico/ Peticion mal formada");
+            throw new Exception(" La peticion esta mal formada");
         }
         if (!selector.containsKey(resource)){
-            return selector.get("err");
+            return selector.get("error");
         }else{
             return selector.get(resource);
         }
